9f8c770add481fd8d247ba893b4c26d3adaf171a
anmol2609 <anmoldwivedi145@gmail.com>
2024-05-06 18:30:17 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+
+import ReactDOM from "react-dom/client";
+const App = () =>{
+    return(
+        <div className="app">code start</div>
+    )
+}
+const root =ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App/>)
